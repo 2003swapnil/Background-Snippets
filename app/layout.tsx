93672d04011c001715e883aeb12965944a98a261
Swapnil Kshirsagar <156304010+2003swapnil@users.noreply.github.com>
2025-07-04 20:52:40 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,29 +5,29 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'BG.IBELICK - Modern, Ready-to-Use Background Snippets for Developers',
+  title: 'Ready-to-Use Background Snippets for Developers',
   description:
     'A collection of modern background snippets for web developers. Ready-to-use snippets crafted with Tailwind CSS and Vanilla CSS for seamless integration.',
   keywords:
     'background snippets, Tailwind CSS, CSS snippets, web development, front-end snippets, free CSS resources',
   openGraph: {
-    title: 'BG.IBELICK - Ready-to-Use Background Snippets',
+    title: 'Ready-to-Use Background Snippets for Developers',
     description:
       'Modern background snippets for web developers crafted with Tailwind CSS and Vanilla CSS.',
-    url: 'https://bg.ibelick.com',
+
     type: 'website',
     images: [
       {
         url: '/banner.jpg',
         width: 1200,
         height: 630,
-        alt: 'BG.IBELICK Cover Image',
+        alt: ' Cover Image',
       },
     ],
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'BG.IBELICK - Ready-to-Use Background Snippets',
+    title: 'Ready-to-Use Background Snippets',
     description:
       'Modern background snippets for web developers crafted with Tailwind CSS and Vanilla CSS.',
     image: '/banner.jpg',
